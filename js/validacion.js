658f5180ce8f582c163a2683c8cdb500805a2640
tahirivazquez <tahiwdev@gmail.com>
2023-08-09 20:02:02 -0300

    validaciones
    primera versión funcional

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,24 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+    document.getElementById("regBtn").addEventListener('click',()=>{
+        let vacios = true
+        var inputs = document.getElementsByTagName('input')
+        for (let i = 0; i < inputs.length; i++) {
+            vacios = vacios && inputs[i].value != '';
+        }
+        let largoPassword = document.getElementById('password1').value.length >= 6
+        let passwordIguales = document.getElementById('password1').value == document.getElementById('password2').value
+        let condicionesCheck = document.getElementById('terminos').checked
+        console.log(vacios, largoPassword, passwordIguales, condicionesCheck)
+
+
+        if (vacios && largoPassword && passwordIguales && condicionesCheck) {
+            event.preventDefault()
+            showAlertSuccess()
+        }else{
+            event.preventDefault();
+            showAlertError()
+        }
+    });
